5a112aa0f5def2507d7e59f23c58c43f1ba4e089
Mika Senghaas <mail@mikasenghaas.de>
2024-03-15 10:52:07 +0100

    feat(sh) updated bash script

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
 #SBATCH --qos cs413
 
 echo "Loading modules"
-module load gcc python py-torch
+module load gcc python openmpi py-torch
 
 echo "Loading venv"
 virtualenv --system-site-packages ~/venvs/neural-style-pt
@@ -20,7 +20,10 @@ echo "Installing dependencies"
 pip install numpy pillow torch torchvision
 
 echo "Running image synthesis"
-python neural-style-pt -style-image examples/inputs/cinestill1.jpg -content-image examples/inputs/epfl.jpg -output-image examples/outputs/epfl1.jpg
+python neural_style.py -style-image examples/inputs/cinestill1.jpg -content-image examples/inputs/epfl.jpg -output-image examples/outputs/epfl1.jpg
+python neural_style.py -style-image examples/inputs/cinestill2.jpg -content-image examples/inputs/epfl.jpg -output-image examples/outputs/epfl2.jpg
+python neural_style.py -style-image examples/inputs/cinestill1.jpg -content-image examples/inputs/trains.jpg -output-image examples/outputs/trains1.jpg
+python neural_style.py -style-image examples/inputs/cinestill2.jpg -content-image examples/inputs/trains.jpg -output-image examples/outputs/trains2.jpg
 
 # to stop venv
 deactivate
